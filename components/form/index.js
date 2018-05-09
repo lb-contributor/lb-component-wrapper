42dbb84c55d7b071db560d02fe9b73f76a7ca402
dengxiaodi <dengxiaodi@163.com>
2018-05-09 16:45:47 +0800

    增加form.SimpleFormItem

--- components/form/index.js
+++ components/form/index.js
@@ -1,3 +1,41 @@
 import { Form } from 'antd'
 
+export const { Item } = Form
+
+const FIELD_META_PROP = 'data-__meta'
+const FIELD_DATA_PROP = 'data-__field'
+const VALUE = 'value'
+const OPTIONS = 'options'
+const CHECKED = 'checked'
+
+
+export class SimpleFormItem extends Form.Item {
+  shouldComponentUpdate(nextProps) {
+    const currField = this.newGetField(this.props)
+    const nextField = this.newGetField(nextProps)
+
+    if (!currField || !nextField) {
+      return true
+    }
+
+    const ret = this.newGetChildProps(this.props, VALUE) !== this.newGetChildProps(nextProps, VALUE) ||
+      this.newGetChildProps(this.props, OPTIONS) !== this.newGetChildProps(nextProps, OPTIONS) ||
+      this.newGetChildProps(this.props, CHECKED) !== this.newGetChildProps(nextProps, CHECKED) ||
+      currField.validating !== nextField.validating ||
+      currField.value !== nextField.value ||
+      currField.error !== nextField.error ||
+      currField.initialValue !== nextField.initialValue
+    return ret
+  }
+
+  newGetChildProps = (props, prop) => {
+    const child = this.getControls(props.children, false)[0]
+    return child && child.props && child.props[prop]
+  }
+
+  newGetMeta = props => this.newGetChildProps(props, FIELD_META_PROP)
+
+  newGetField = props => this.newGetChildProps(props, FIELD_DATA_PROP)
+}
+
 export default Form
