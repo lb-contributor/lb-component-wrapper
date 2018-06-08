6260979b19a103c76cba5b2abc1b6329b4a588ab
deng xiaodi <dengxiaodi@gmail.com>
2018-06-08 11:09:55 +0800

    增加export DateRange等

--- components/datePicker/index.jsx
+++ components/datePicker/index.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { DatePicker } from 'antd'
 
+export const { MonthPicker, RangePicker, WeekPicker } = DatePicker
+
 class DatePickerWrapper extends Component {
   // FIXME 补充完整的刷新条件
   shouldComponentUpdate(nextProps, nextState) {
