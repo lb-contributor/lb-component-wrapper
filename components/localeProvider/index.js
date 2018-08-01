30163c3c26fa20886b68d2e67a392b8eeddf35ff
chenlin <jiay0u@yahoo.com>
2018-08-01 13:56:30 +0800

    add localeProvider & upload utils interfaces

---
+++ components/localeProvider/index.js
@@ -0,0 +1,6 @@
+import LocaleReceiver from 'antd/lib/locale-provider/LocaleReceiver'
+import defaultLocale from 'antd/lib/locale-provider/default'
+import LocaleProvider from 'antd/lib/locale-provider/index'
+
+export {defaultLocale, LocaleReceiver}
+export default LocaleProvider
